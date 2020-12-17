9164eeeca7b80a9e60009f162cc477a25296cd10
Alfredo <alfre.jangeles@hotmail.com>
2020-12-17 02:29:34 -0600

    Realizando construcción de commentarios con json

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,22 @@
+class CommentsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def create
+  	#binding.pry
+  	@article = Article.find(params[:article_id])
+    @comment = @article.comments.create(commenter: params[:commenter],body: params[:body])
+    #redirect_to article_path(@article)
+    @message = "llegué"
+    respond_to do |format|
+        format.json{render json: { message: @message }, status: 200} 
+    end
+  end
+
+  #private
+   # def comment_params
+    #  params.require(:comment).permit(:commenter, :body)
+    #end
+end
+
+
+     
